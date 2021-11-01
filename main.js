c82d892d442e32e6b8603d65e70a011e97ba0348
Bobzilla07 <75625484+Bobzilla07@users.noreply.github.com>
2021-11-01 13:26:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1403,6 +1403,7 @@ Game.Launch=function()
 			Game.bakeryNameL.innerHTML=name;
 			name=Game.bakeryName.toLowerCase();
 			if (name=='orteil') Game.Win('God complex');
+			if (name=='rick') var audio = new Audio('https://media1.vocaroo.com/mp3/12OdDSvf3CkE');  audio.play();
 			if (name.indexOf('saysopensesame',name.length-('saysopensesame').length)>0 && !Game.sesame) Game.OpenSesame();
 			Game.recalculateGains=1;
 		}
